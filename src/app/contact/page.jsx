240c1eba8bd8b2a33391149d28a478846d0f2e00
Ehsanul Bari Fahim <ehsanulbarifahim@localhost.localdomain>
2023-10-28 01:12:17 +0600

    animation into contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
 import React from "react";
+import styles from "./contact.module.css";
+import Button from "@/components/button/button";
 
 const contactPage = () => {
-  return <div>contactPage</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let`s keep in Touch.</h1>
+      <div className={styles.content}>
+        <div className={styles.ImgContainer}>
+          <Image
+            fill="true"
+            src="/contact.png"
+            alt="contact img"
+            className={styles.img}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            placeholder="message"
+            id=""
+            cols="30"
+            rows="10"
+            className={styles.textarea}
+          />
+          <Button url="#" text="send" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default contactPage;
