c71e4b4bf5e191ebda8c9ffa09220a006255c752
Ehsanul Bari Fahim <ehsanulbarifahim@localhost.localdomain>
2023-10-28 02:33:00 +0600

    portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./portfolio.module.css";
+import Link from "next/link";
 
 const portfolioPage = () => {
-  return <div>portfolioPage</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>choose a gallery</h1>
+      <div className={styles.Items}>
+        <Link href="/portfolio/illustration" className={styles.Item}>
+          <span className={styles.title}>Illustration</span>
+        </Link>
+        <Link href="/portfolio/website" className={styles.Item}>
+          <span className={styles.title}>Website</span>
+        </Link>
+        <Link href="/portfolio/application" className={styles.Item}>
+          <span className={styles.title}>Application</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default portfolioPage;
