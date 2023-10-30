8e0c6be57cc8259d98a154e1aa99b7630b3a0cd8
Ehsanul Bari Fahim <ehsanulbarifahim@localhost.localdomain>
2023-10-30 16:31:36 +0600

    dynamic catagory of portfolio

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,77 @@
+import Button from "@/components/button/button";
 import React from "react";
+import styles from "./category.module.css";
+import freshFruits from "../../../../public/fresh Fruts.jpg";
+import Image from "next/image";
 
-const Category = () => {
-  return <div>Category</div>;
+const Category = ({ params }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.Title}> {params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.textTitle}>Test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit
+            <br />
+            repellat iure minus eos incidunt enim fuga nemo atque perspiciatis
+            quaerat.
+          </p>
+          <Button text="see more" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src={freshFruits}
+            alt="freshFruits"
+          />
+        </div>
+      </div>
+      {/* second div */}
+      <div className={styles.item}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.textTitle}>Test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit
+            <br />
+            repellat iure minus eos incidunt enim fuga nemo atque perspiciatis
+            quaerat.
+          </p>
+          <Button text="see more" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src={freshFruits}
+            alt="freshFruits"
+          />
+        </div>
+      </div>
+      {/* third div */}
+      <div className={styles.item}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.textTitle}>Test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit
+            <br />
+            repellat iure minus eos incidunt enim fuga nemo atque perspiciatis
+            quaerat.
+          </p>
+          <Button text="see more" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src={freshFruits}
+            alt="freshFruits"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
--- src/app/portfolio/layout.jsx
+++ src/app/portfolio/layout.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./portfolio.module.css";
+import Link from "next/link";
 
 const portfolioLayout = ({ children }) => {
   return (
     <div>
-      <h1 className={styles.mainTitle}>Our Works</h1>
+      <h1 className={styles.mainTitle}>
+        <Link href="/portfolio">Our Works</Link>
+      </h1>
       {children}
     </div>
   );
